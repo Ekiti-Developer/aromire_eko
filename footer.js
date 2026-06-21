1227628ef9418d2fcc847d396d1a0a10ae0248a4
Falusi Oluwatosin <64169670+Ekiti-Developer@users.noreply.github.com>
2026-06-21 18:11:18 +0100

    Remove programs and get involved sections from footer
    Removed program and involvement sections from footer.

--- footer.js
+++ footer.js
@@ -24,24 +24,8 @@
           <li><a href="contact.html">Contact</a></li>
         </ul>
       </div>
-      <div class="footer-col">
-        <h4>Programs</h4>
-        <ul>
-          <li><a href="programs.html">Future Leaders Academy</a></li>
-          <li><a href="programs.html">Women in Business</a></li>
-          <li><a href="programs.html">Health Outreach</a></li>
-          <li><a href="programs.html">Education Support</a></li>
-        </ul>
-      </div>
-      <div class="footer-col">
-        <h4>Get Involved</h4>
-        <ul>
-          <li><a href="contact.html">Donate</a></li>
-          <li><a href="contact.html">Volunteer</a></li>
-          <li><a href="contact.html">Partner With Us</a></li>
-          <li><a href="contact.html">Contact Us</a></li>
-        </ul>
-      </div>
+      
+      
     </div>
     <div class="footer-bottom"> <p class="footer-copy">© ${new Date().getFullYear()} Aromire-Éko Empowerment Foundation. All rights reserved.</p>
       <div class="footer-legal">
